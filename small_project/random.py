7db0e6fcfbd0d0a3fd9614b084617e5a20d8cfd9
iambramhaa <iamashokaryal@gmail.com>
2024-11-06 19:09:36 +0545

    commit

--- small_project/random.py
+++ small_project/random.py
@@ -1,6 +1,34 @@
 
-import random 
+# class Buy:
 
-random_number = random.randint(1,3)
+#     def price_input(self):
 
-print(random_number)
+#         self.item_prices = []
+
+#         self.no_of_item = int(input("Enter the no of items: \n"))
+
+#         for i in range(1,self.no_of_item+1):
+
+#             price = int(input(f"Enter item {i} price: \n"))
+
+#             self.item_prices.append(price)
+
+        
+#     def price_calculation(self):
+
+#         self.total_amount = sum(self.item_prices)
+
+
+#     def print_result(self):
+
+#         print(f"The amount you need to pay is : {self.total_amount}")
+
+# buy = Buy()
+
+# buy.price_input()
+
+# buy.price_calculation()
+
+# buy.print_result()
+
+        
