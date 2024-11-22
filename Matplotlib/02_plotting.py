ebe17ff7bfa7c382f1df6bcc06c6a3a46fe2dc68
iambramhaa <iamashokaryal@gmail.com>
2024-11-22 16:08:08 +0545

    New files

---
+++ Matplotlib/02_plotting.py
@@ -0,0 +1,4 @@
+x = int(input('x:'))
+y = int(input('y:'))
+a = x + y 
+print(a)
---
+++ automate.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+while True:
+
+    time.sleep(10)
+    pyautogui.typewrite('I am very very sorry mero mutu 😥')
+    pyautogui.press('enter')
